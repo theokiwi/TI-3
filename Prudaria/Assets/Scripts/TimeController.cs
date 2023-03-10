cfa46f0dd4f5a15e8ae6946bcb1d2ef8b7d7064a
Theo Diniz~ <theodordiniz@gmail.com>
2023-03-10 15:26:23 -0300

    Basico do Tempo Funcionando
    O tempo passa e tem como acelerar. (nao arthur eu nao usei coroutine voce nao pode me impedir de tacar tudo no update pq eu nao sei mexer em coroutine)

--- Prudaria/Assets/Scripts/TimeController.cs
+++ Prudaria/Assets/Scripts/TimeController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,33 +5,26 @@ using TMPro;
 
 public class TimeController : MonoBehaviour
 {
-    [SerializeField] public float timeMultiplier;
-    [SerializeField] public float startHour;
-    [SerializeField] public TextMeshProUGUI timeText;
-    public string endHour = "22:00";
-    public DateTime currentTime;
-    
-
-    public enum Days
-    {
-        Segunda, Terça, Quarta, Quinta, Sexta
-    }
-
-    public Days currentDay;
+    private float timer = 0.00f;
+    private float endHour = 300.00f; //cinco minutos
+    public float timeMultiplier = 1.00f; // permite acelerar o tempo no inspetor, pra ele nao fazer diferença tem que deixar em 1
     void Start()
     {
-        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+
     }
 
     void Update()
     {
-        UpdateTimeOfDay();
+        timer += Time.deltaTime * timeMultiplier;
+        float seconds = timer % 60;
+        Debug.Log(Mathf.RoundToInt(seconds));
+
+        if (seconds > endHour && seconds < endHour + 0.01f)
+        {
+            timer = 0; //reseta o dia que é equivalente a passar pro proximo dia.
+        }
     }
-    public void UpdateTimeOfDay()
-    {
-        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
-        timeText.text = currentTime.ToString("HH:mm"); //sugeriram colocar dentro de um if(texto != null) mas não entendi pq então não fiz
-    }
+
 }
 
 
